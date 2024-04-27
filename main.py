7de344fd276c148010ed21b987e4c4b3a1ac1229
cbkwgl <cbk123@gmail.com>
2024-04-27 21:09:57 +0100

    Include Store Product and Update Shipment - Actual Quantity

--- main.py
+++ main.py
@@ -2,18 +2,44 @@ import flet as ft
 import sqlite3
 import datetime
 
+# What's Pending?
+# 1. SHIPMENT_INBOUND's Actual Receipt is not getting updated
+# 2. Audit Updates for Over GR and Non Advised Receipt
+
 
 def main(page: ft.Page):
+
+    SITE = "0001"
+    # Define tables - SHIPMENT_INBOUND, GOODS_RECEIPT, AUDIT, PRODUCT_QTY - Extend the logic to include site.
     connection = sqlite3.connect("wms.db", check_same_thread=False)
     cursor = connection.cursor()
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS GOODS_RECEIPT (ASN TEXT, PRODUCT TEXT, QUANTITY INTEGER)"
+        """
+        CREATE TABLE IF NOT EXISTS SHIPMENT_INBOUND 
+        (SITE TEXT NOT NULL, ASN TEXT NOT NULL, PRODUCT TEXT NOT NULL, QUANTITY INTEGER DEFAULT 0, QUANTITY_RECEIVED INTEGER DEFAULT 0,
+        CONSTRAINT PK_SHP_INB PRIMARY KEY (SITE, ASN, PRODUCT))
+        """
+    )
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS GOODS_RECEIPT 
+        (SITE TEXT NOT NULL, ASN TEXT NOT NULL, PRODUCT TEXT NOT NULL, QUANTITY INTEGER DEFAULT 0)
+        """
     )
     cursor.execute(
         """CREATE TABLE IF NOT EXISTS AUDIT 
-        (TIME_STAMP INT, TRANS_DATE STRING, TRANS_TIME STRING, TRANS_TYPE TEXT, TRANS_SUBTYPE TEXT, 
-        REASON_CODE TEXT, REFERENCE TEXT, PRODUCT TEXT, QUANTITY INTEGER)"""
+        (TIME_STAMP INT, TRANS_DATE TEXT, TRANS_TIME TEXT, SITE TEXT, TRANS_TYPE TEXT, TRANS_SUBTYPE TEXT, 
+        REASON_CODE TEXT, REFERENCE TEXT, PRODUCT TEXT, QUANTITY INTEGER)
+        """
     )
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS PRODUCT_QTY 
+        (SITE TEXT NOT NULL, PRODUCT TEXT NOT NULL, QUANTITY_AVAILABLE INTEGER DEFAULT 0, QUANTITY_QUALITY INTEGER DEFAULT 0, 
+        QUANTITY_ORDERED INTEGER DEFAULT 0, QUANTITY_PICKED INTEGER DEFAULT 0, QUANTITY_TO_DESPATCH INTEGER DEFAULT 0,
+        CONSTRAINT PRODUCT_QTY_PRIMARY PRIMARY KEY (SITE, PRODUCT))
+        """
+    )
+
+    # Test Data: This data should come from reading an XML batch which comes from a queue
 
     page.adaptive = True
     t = ft.Text(value="Enter Shipment Number", color="cyan", size=33)
@@ -59,14 +85,15 @@ def main(page: ft.Page):
                 + text_field2b.value
             )
         )
-        entry = (text_field1.value, text_field2a.value, text_field2b.value)
+        entry = (SITE, text_field1.value, text_field2a.value, text_field2b.value)
         new_task.value = ""
-        cursor.execute("INSERT INTO GOODS_RECEIPT VALUES (?, ?, ?)", entry)
+        cursor.execute("INSERT INTO GOODS_RECEIPT VALUES (?, ?, ?, ?)", entry)
         connection.commit()
         audit_entry = (
             int(datetime.datetime.now().strftime("%Y%m%d%H%M%S")),
             datetime.datetime.now().strftime("%d-%m-%Y"),
             datetime.datetime.now().strftime("%H:%M:%S"),
+            SITE,
             "RECEIPT",
             "",
             "",
@@ -75,9 +102,42 @@ def main(page: ft.Page):
             text_field2b.value,
         )
         cursor.execute(
-            "INSERT INTO AUDIT VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", audit_entry
+            "INSERT INTO AUDIT VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", audit_entry
         )
         connection.commit()
+        shipment_inbound_update_entry = (
+            text_field2b.value,
+            SITE,
+            text_field1.value,
+            text_field2a.value,
+        )
+        print(shipment_inbound_update_entry)
+        cursor.execute(
+            """UPDATE SHIPMENT_INBOUND 
+            SET QUANTITY_RECEIVED = QUANTITY_RECEIVED + ? 
+            WHERE SITE = ? AND ASN = ? AND PRODUCT = ?
+            """,
+            shipment_inbound_update_entry,
+        )
+        connection.commit()
+        site_product = (SITE, text_field2a.value)
+
+        cursor.execute(
+            """
+            INSERT OR IGNORE INTO product_qty (site, product) values (?,?)
+            """,
+            site_product,
+        )
+        connection.commit()
+        site_product_qty = (text_field2b.value, SITE, text_field2a.value)
+        cursor.execute(
+            """
+            UPDATE product_qty 
+            SET quantity_available = quantity_available + ? WHERE site = ? and product = ?
+            """,
+            site_product_qty,
+        )
+
         page.update()
 
     new_task = ft.Row(controls=[text_field2a, text_field2b], visible=False)
--- sqlread.py
+++ sqlread.py
@@ -2,6 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect("wms.db", check_same_thread=False)
 cursor = connection.cursor()
+print("GOODS RECEIPT")
 cursor.execute("SELECT * FROM GOODS_RECEIPT")
 
 rows = cursor.fetchall()
@@ -9,7 +10,22 @@ rows = cursor.fetchall()
 for row in rows:
     print(row)
 
+print("AUDIT")
 cursor.execute("SELECT * FROM AUDIT")
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+print("PRODUCT QTY")
+cursor.execute("SELECT * FROM PRODUCT_QTY")
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+print("SHIPMENT")
+cursor.execute("SELECT * FROM SHIPMENT_INBOUND")
 rows = cursor.fetchall()
 
 for row in rows:
